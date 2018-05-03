e3f2008cccebc312b835ee636ea84dc889899e6f
nayanam08 <nayana.naan@gmail.com>
2018-05-03 14:17:21 +0200

    Learn basics

---
+++ ll_env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/nmanniltho001/Documents/learnpython/LearningLog/ll_env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
