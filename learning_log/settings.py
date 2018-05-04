b3a69b44e98a46aa6d577f3013edb97af4e3d3db
nayanam08 <nayana.naan@gmail.com>
2018-05-04 15:23:45 +0200

    rough work

--- learning_log/settings.py
+++ learning_log/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'bootstrap3',
     # My apps
     'learning_logs',
+    'users',
 
 ]
 
--- learning_log/urls.py
+++ learning_log/urls.py
@@ -19,5 +19,6 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^', include('learning_logs.urls')),
+    url(r'^users/', include(('users.urls', 'users'), namespace='users')),
+    url(r'^', include(('learning_logs.urls', 'learning_logs' ), namespace = 'learning_logs')),
 ]
